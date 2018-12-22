aaf1f53a0a5379220df0e05d4febd7c01f3e1bf9
sanie-i <sanie96@gmail.com>
2018-12-22 23:09:42 +0100

    add prefix to css
    + clean up index.js

--- js/index.js
+++ js/index.js
@@ -14,8 +14,8 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+//smooth scrolls to figure
 $(document).ready(function(){
-  //smooth scrolls to figure
     $(function() {
     $('.fig').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -74,13 +74,4 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    // document.body.style.backgroundColor = "yellow";
-  } else {
-   // document.body.style.backgroundColor = "pink";
-  }
-}
-
-myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
