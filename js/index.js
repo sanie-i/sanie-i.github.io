e61e36bba38bee0f03e317bf403d08c86281ef3f
sonya-irsay <sanie@newschool.edu>
2019-11-04 10:38:28 -0500

    update js

--- js/index.js
+++ js/index.js
@@ -10,8 +10,10 @@ for (i = 0; i < coll.length; i++) {
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
+      this.style.paddingBottom = 0;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+      this.style.paddingBottom = 0.5 + "em";
     }
   });
 }
